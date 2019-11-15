998dbc29c3e4d0f1405d09ab021095a0932909ef
Dariush Abbasi <dariushem@yahoo.com>
2019-11-15 22:01:48 +0330

    handle ctrl+c for better exit

--- repl.c
+++ repl.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
 
+static volatile int keepalive = 1;
+
+void sig_handler(int d);
+
 int repl() {
+
+    signal(SIGINT, sig_handler);
+
     char *command;
-    for(;;) {        
+    while(keepalive) {        
         command = readline("(meow)> ");
         add_history(command);
         
@@ -17,4 +26,10 @@ int repl() {
 
         fprintf(stdout, "%s\n", command);
     }
+}
+
+void sig_handler(int d) {
+    keepalive = 0;
+    fprintf(stdout, " by 🐱\n");
+    exit(0);
 }
